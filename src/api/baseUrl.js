f477b6884a51a0ce3502377788bd521d16f4a996
ex_chenglinxing <ex_chenglinxing@axatp.com>
2021-07-13 17:14:54 +0800

    修改

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,5 @@
 const api = {
-    'http://localhost:8081': 'http://localhost:5000', //本地指向后端
-    'http://10.138.123.33:8080': 'http://10.138.123.33:5000', //本地指向后端
-
+    'http://localhost:8080': 'http://localhost:5000', //本地指向后端
 }
 
 export const baseUrl = api[window.location.origin]
