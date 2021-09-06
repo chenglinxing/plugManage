1507f9709649d60fe7fd027a37a9245ce1a73993
chenglinxing <1428054498@qq.com>
2021-09-06 16:34:03 +0800

    修改映射

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,7 @@
 const api = {
     'http://localhost:8080': 'http://localhost:5000', //本地指向后端
+	'http://47.100.93.30:5000':'http://47.100.93.30:5000',
+	'http://localhost:8080': 'http://47.100.93.30:5000',
 }
 
 export const baseUrl = api[window.location.origin]
